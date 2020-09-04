fd4e50bf5b042e3680add6dff14ee66e94cacb7c
Diego <diego.capoccitti@gmail.com>
2020-09-04 16:45:37 +0200

    a verde e fontsize

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,8 +16,8 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::red);
+    g.setFont (juce::Font (32.0f));
+    g.setColour (juce::Colours::green);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
